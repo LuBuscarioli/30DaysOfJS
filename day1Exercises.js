d77af5d572cfdc7655d580406ea955c2c171151c
Lucas-Buscarioli <lucas.buscarioli@gmail.com>
2024-03-19 17:46:59 -0300

    day 2 ex until 13

---
+++ day1Exercises.js
@@ -0,0 +1,22 @@
+/*
+
+DAY 01 LINK:
+https://github.com/Asabeneh/30-Days-Of-JavaScript/blob/master/readMe.md
+
+*/
+
+let nameStr = "Lucas"
+let age = 28
+let person = true
+let empty
+
+console.log (typeof nameStr)
+console.log (typeof age)
+console.log (typeof person)
+console.log(typeof empty)
+
+let firstName = 'Lucas', lastName = "Buxk", maritageStatus = 'Single', country = "BR", myAge = 29
+
+console.log(maritageStatus, country)
+
+console.log(`I am ${myAge} YO. You are ${age}.`)
---
+++ day2Exercises.js
@@ -0,0 +1,18 @@
+let challenge = '30 Days Of JavaScript'
+let brands = 'Facebook, Microsoft, nVidia, AMD'
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+
+console.log(challenge.substr(3, 4))
+console.log(challenge.substring(3, 7))
+console.log(challenge.substring(3))
+console.log(challenge.includes('Script'))
+
+console.log(challenge.split())
+console.log(challenge.split(' '))
+console.log(brands.split(','))
+
+console.log(challenge.replace('JavaScript', 'Phyton'))
+console.log(challenge.charAt(15))
